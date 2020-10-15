4291963e71f63e0e4d58ba4c3a4bfdcd97969931
Daniel Suo <danielsuo@gmail.com>
2020-10-14 21:55:40 -0400

    Add rendering

--- deluca/envs/lung/core.py
+++ deluca/envs/lung/core.py
@@ -1,6 +1,20 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 from deluca.envs.core import Env
 
 
 class Lung(Env):
+    """
+    Todos:
+        - Plot incrementally
+        - Save fig as fig.self
+        - Modify close to delete fig and accumulated data
+    """
+
     def render(self):
-        pass
+        fig, ax = plt.subplots()
+        buf, (nrow, ncol) = fig.canvas.print_to_buffer()
+        rgb_data = np.frombuffer(buf, dtype=np.uint8).reshape(nrow, ncol, 4)[:, :, :3]
+
+        return rgb_data
